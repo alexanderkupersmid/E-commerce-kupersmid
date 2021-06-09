f6304ebc07eebe36cf4696847330cc6ca1e5db04
alexanderkupersmid <alexanderkupersmid@gmail.com>
2021-06-09 19:43:38 -0300

    hahaha

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React from 'react';
+import {NavBarStyle} from './NavbarStyle';
+import {CartWidget} from '../CartWidget/CartWidget';
 import {Button, AppBar, Toolbar, Typography, makeStyles} from '@material-ui/core';
+import { createMuiTheme } from '@material-ui/core/styles';
 
-
-
+//como debo importar el logo desde la carpeta image?
+//probe con ../../image/logo.png pero me dio error.
 
 const useStyles = makeStyles(theme=>({
   offset: theme.mixins.toolbar,
   titulo:{flexGrow: 1},
-
 }))
 
 
@@ -16,13 +18,14 @@ export default function NavBar() {
   const classes = useStyles()
   return (
     <div>
-      <AppBar position="fixed" color="primary">
-        <Toolbar>
+      <AppBar position="fixed" color="secondary" >
+        <Toolbar> 
           <Typography variant="h6" color="inherit" className={classes.titulo}>
             RAME Kids
           </Typography>
           <Button variant="text" color="inherit">Home</Button>
           <Button variant="text" color="inherit">Contact</Button>
+          <CartWidget />
         </Toolbar>
       </AppBar>
     <div className={classes.offset}> </div>
