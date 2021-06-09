508f400676ae7e00c82a076ce96716af4ed6df33
alexanderkupersmid <77647546+alexanderkupersmid@users.noreply.github.com>
2021-06-09 16:53:14 -0300

    Update NavBar.jsx
    cambios en el navbar

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React from 'react';
+import {NavBarStyle} from './NavbarStyle';
+import {CartWidget} from '../CartWidget/CartWidget';
 import {Button, AppBar, Toolbar, Typography, makeStyles} from '@material-ui/core';
+import { createMuiTheme } from '@material-ui/core/styles';
 
-
-
+//como debo importar el logo desde la carpeta image?
+//probe con ../../image/logo.png pero me dio error.
 
 const useStyles = makeStyles(theme=>({
   offset: theme.mixins.toolbar,
@@ -23,6 +26,7 @@ export default function NavBar() {
           </Typography>
           <Button variant="text" color="inherit">Home</Button>
           <Button variant="text" color="inherit">Contact</Button>
+          <CartWidget />
         </Toolbar>
       </AppBar>
     <div className={classes.offset}> </div>
